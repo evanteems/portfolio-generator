61a2427f381a706bddb283bcd9a1b95782e79a85
Evan Teems <evanteems40@yahoo.com>
2021-02-12 22:15:53 -0800

    answers

--- app.js
+++ app.js
@@ -70,6 +70,44 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
+      "name": "leranatino",
+      "github": "lernantino",
+      "confirmAbout": true
+    }
+  ])
   console.log(portfolioData);
 })
 .then(projectAnswers => console.log(projectAnswers))
