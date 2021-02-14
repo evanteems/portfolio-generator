c50e12bdf768c780ff24b0eb92adbe3748a6ec0d
Evan Teems <evanteems40@yahoo.com>
2021-02-13 22:02:17 -0800

    prompt edit

---
+++ dist/generate-site.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File Created!'
+            });
+        });
+    });
+};
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Copy Success!'
+            });
+        });
+    });
+};
+
+module.exports = {
+    writeFile: writeFile,
+    copyFile: copyFile
+};
